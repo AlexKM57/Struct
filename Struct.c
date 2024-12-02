b1c85f27f5cd6df1418795d5d107ce6875df1d99
AlexKM57 <komcinclair@gmail.com>
2024-12-02 03:38:22 +0100

    debut du projet

---
+++ Struct.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "struct.h"
+
+
+
+
+int main(){
+
+	Booleen start = ENCOUR;
+	printf("%d\n",start);
+	   
+	joueur p1 = {"", 0, 0};
+
+	create_player(&p1);
+
+	printf("le joueur 1 est  %s avec %d hp et %d mp\n", p1.name, p1.hp, p1.mp);
+
+    return 0;
+}
+
+void create_player (joueur *p1)
+{
+	strcpy(p1->name, "Alex");
+	p1->hp = 100;
+	p1->mp = 100;
+} 
+
+/*
+dans les structure pour le pointeur d'une variable on utilise le fleche (->)
+EX: p1->hp designe l'adresse le hp du joueur 1
+
+
+*/
---
+++ struct.h
@@ -0,0 +1,23 @@
+
+#ifndef __HAUT_H__
+#define __HAUT_H__
+
+//le typedef permet de transformer une structure ou une ennumeration en type
+typedef struct player
+{
+	signed char name[256];
+	int hp;
+	int mp;
+}joueur;
+
+typedef enum booleen
+{
+	NON,//0
+	OUI,//1
+	ENCOUR//2
+}Booleen;
+
+
+void create_player (joueur *p1);
+
+#endif
